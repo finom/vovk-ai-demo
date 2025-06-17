5bed7357b3c4f549a4bf682b14ab5e7af3035531
Andrey Gubanov <andrii@gubanov.eu>
2025-06-17 10:22:01 +0200

    fix: Deployment fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,9 @@
 import Board from "@/components/Board";
 import { ExpandableChatDemo } from "@/components/ExpandableChatDemo";
 import UserList from "@/components/UserList";
-import { UserModelType } from "@/registry";
-import { UserRPC } from "vovk-client";
 
 export default async function Home() {
-  const initialData = await UserRPC.getUsers<UserModelType[]>();
+  const initialData = await [];
 
   return (
     <>
