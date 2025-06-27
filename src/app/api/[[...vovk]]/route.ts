c1a970da208410d754d0cedfb9f6d1db1393dad7
Andrey Gubanov <andrii@gubanov.eu>
2025-06-27 23:09:23 +0200

    fix

--- src/app/api/[[...vovk]]/route.ts
+++ src/app/api/[[...vovk]]/route.ts
@@ -18,4 +18,5 @@ export type Controllers = typeof controllers;
 export const { GET, POST, PATCH, PUT, HEAD, OPTIONS, DELETE } = initSegment({
   emitSchema: true,
   controllers,
+  onError: console.error,
 });
