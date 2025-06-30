c4e4caf8c3b8b84f0b4571412009e825ae7c56e1
Andrey Gubanov <andrii@gubanov.eu>
2025-06-30 21:35:39 +0200

    fix

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -5,12 +5,17 @@ export const fetcher = createFetcher({
   transformResponse: (data) => {
     const state = useRegistry.getState();
     if(Symbol.asyncIterator in data) {
-      return async function* () {
+      async function* generator() {
         for await (const item of data) {
           state.parse(item); // parse each item in the async iterable
           yield item;
         }
       }
+      
+      generator[Symbol.dispose] = data[Symbol.dispose];
+      generator[Symbol.asyncDispose] = data[Symbol.asyncDispose];
+
+      return generator;
     } 
     
     state.parse(data); // parse regular JSON data
