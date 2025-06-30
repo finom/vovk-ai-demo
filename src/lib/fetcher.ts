7f9ef008441de8578adde50f551223ece522471b
Andrey Gubanov <andrii@gubanov.eu>
2025-06-30 21:55:41 +0200

    fix

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -2,20 +2,11 @@ import { useRegistry } from "@/registry";
 import { createFetcher } from "vovk";
 
 export const fetcher = createFetcher({
-  transformResponse: (data) => {
+  transformResponse: async (data) => {
     const state = useRegistry.getState();
-    if(Symbol.asyncIterator in data) {
-      async function* generator() {
-        for await (const item of data) {
-          state.parse(item); // parse each item in the async iterable
-          yield item;
-        }
-      }
-      
-      generator[Symbol.dispose] = data[Symbol.dispose];
-      generator[Symbol.asyncDispose] = data[Symbol.asyncDispose];
-
-      return generator;
+    if(Symbol.asyncIterator in data && 'onIterate' in data && typeof data.onIterate === 'function') {
+      data.onIterate(state.parse); // handle each item in the async iterable
+      return data;
     } 
     
     state.parse(data); // parse regular JSON data
