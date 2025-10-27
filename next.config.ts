a0f52e42fa56275ae50c985206927d5333785207
Andrey Gubanov <andrii@gubanov.eu>
2025-10-27 13:46:24 +0400

    ci: Remove @prisma/nextjs-monorepo-workaround-plugin as it doesn't work with turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from "next";
-// @ts-expect-error PrismaPlugin doesn't have types
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
-const nextConfig: NextConfig = {
-    webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()]
-    }
-
-    return config
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
