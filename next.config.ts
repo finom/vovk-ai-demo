ff9d843b34bf562015cb9ecfc7a90762297793bf
Andrey Gubanov <andrii@gubanov.eu>
2025-10-27 13:42:23 +0400

    ci: Fix

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
+// @ts-expect-error PrismaPlugin doesn't have types
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
+};
 
 export default nextConfig;
